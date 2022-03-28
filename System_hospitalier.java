3aa83b91281bbdaa2218c05b2e8d70bd69679d7e
TNEngie <fayetjules@gmail.com>
2022-03-28 10:39:57 +0200

    Ajout commentaires

---
+++ System_hospitalier.java
@@ -0,0 +1,52 @@
+package tp_git;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class System_hospitalier {
+	
+	private List<Requête> listRequetes;
+
+	public System_hospitalier(List<Requête> listRequetes) {
+		super();
+		this.listRequetes = listRequetes;
+	}
+
+	public System_hospitalier() {
+		super();
+		listRequetes = new ArrayList<>();
+	}
+
+	public List<Requête> getListRequetes() {
+		return listRequetes;
+	}
+
+	public void setListRequetes(List<Requête> listRequetes) {
+		this.listRequetes = listRequetes;
+	}
+	
+	public void recevoirRequete(Requête requete) {
+		listRequetes.add(requete);
+		String analyse =  analyserRequete(requete);
+		String comparaisonDonnees = comparerDonnes();
+		System.out.println(analyse);
+		System.out.println(comparaisonDonnees);
+	}
+	
+	/**
+	 * Analyse la requête
+	 */
+	private String analyserRequete(Requête requête) {
+		// Analyser la requête
+		return "requête analysée";
+	}
+	
+	/**
+	 * Compare les données
+	 */
+	private String comparerDonnes() {
+		// Comparaison des données
+		return "Cluster trouvé";
+	}
+
+}
